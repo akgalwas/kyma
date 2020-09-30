ff93f10bf74fbe57c39f71475b275f91dffe5d83
franpog859 <franciszek.pog@gmail.com>
2020-09-30 14:59:53 +0200

    Clean up not used things

--- components/system-broker-agent/internal/systemmapping/controller.go
+++ components/system-broker-agent/internal/systemmapping/controller.go
@@ -6,7 +6,6 @@ import (
 	v1alpha12 "github.com/kyma-project/kyma/components/system-broker-agent/pkg/apis/applicationconnector/v1alpha1"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -17,15 +16,13 @@ func NewControllerManagedBy(mgr manager.Manager) error {
 		NewControllerManagedBy(mgr).
 		For(&v1alpha12.SystemMapping{}).
 		Complete(&reconciler{
-			Client: mgr.GetClient(),
-			scheme: mgr.GetScheme(),
+			client: mgr.GetClient(),
 			log:    logrus.WithField("Controller", "SystemMapping"),
 		})
 }
 
 type reconciler struct {
-	client.Client
-	scheme *runtime.Scheme
+	client client.Client
 	log    *logrus.Entry
 }
 
@@ -33,7 +30,7 @@ func (r *reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	r.log.Logf(logrus.InfoLevel, "Reconciling SystemMapping %s", req.NamespacedName)
 
 	sm := &v1alpha12.SystemMapping{}
-	if err := r.Client.Get(context.Background(), req.NamespacedName, sm); err != nil {
+	if err := r.client.Get(context.Background(), req.NamespacedName, sm); err != nil {
 		return ctrl.Result{}, errors.Wrap(err, fmt.Sprintf("failed to get SystemMapping %s", req.NamespacedName))
 	}
 	// TODO: Do something!
