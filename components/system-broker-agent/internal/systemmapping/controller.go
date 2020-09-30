6ce11410f42c13caa1e741561e970fa3b216a555
franpog859 <franciszek.pog@gmail.com>
2020-09-30 13:56:01 +0200

    Log the SystemMapping ServiceIDs just for the sake of testing

--- components/system-broker-agent/internal/systemmapping/controller.go
+++ components/system-broker-agent/internal/systemmapping/controller.go
@@ -1,7 +1,10 @@
 package systemmapping
 
 import (
+	"context"
+	"fmt"
 	v1alpha12 "github.com/kyma-project/kyma/components/system-broker-agent/pkg/apis/applicationconnector/v1alpha1"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -28,7 +31,13 @@ type reconciler struct {
 
 func (r *reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	r.log.Logf(logrus.InfoLevel, "Reconciling SystemMapping %s", req.NamespacedName)
+
+	sm := &v1alpha12.SystemMapping{}
+	if err := r.Client.Get(context.Background(), req.NamespacedName, sm); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, fmt.Sprintf("failed to get SystemMapping %s", req.NamespacedName))
+	}
 	// TODO: Do something!
+	r.log.Logf(logrus.InfoLevel, "SystemMapping's %s ServiceIDs: %v", req.NamespacedName, sm.Spec.ServiceIDs)
 
 	return ctrl.Result{}, nil
 }
