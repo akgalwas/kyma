abb50a961e5c76d3b63dc8c329f9d8462fe2675e
franpog859 <franciszek.pog@gmail.com>
2020-10-08 09:19:08 +0200

    Log the warning instead of returning the error

--- components/system-broker-agent/internal/systemmapping/controller.go
+++ components/system-broker-agent/internal/systemmapping/controller.go
@@ -2,9 +2,7 @@ package systemmapping
 
 import (
 	"context"
-	"fmt"
 	v1alpha12 "github.com/kyma-project/kyma/components/system-broker-agent/pkg/apis/applicationconnector/v1alpha1"
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -31,7 +29,8 @@ func (r *reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	sm := &v1alpha12.SystemMapping{}
 	if err := r.client.Get(context.Background(), req.NamespacedName, sm); err != nil {
-		return ctrl.Result{}, errors.Wrap(err, fmt.Sprintf("failed to get SystemMapping %s", req.NamespacedName))
+		r.log.Logf(logrus.WarnLevel, "Failed to get SystemMapping %s", req.NamespacedName)
+		return ctrl.Result{}, nil
 	}
 	// TODO: Do something!
 	r.log.Logf(logrus.InfoLevel, "SystemMapping's %s ServiceIDs: %v", req.NamespacedName, sm.Spec.ServiceIDs)
